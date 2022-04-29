0c9d67203b550eab9aef6f6b7529445bf24f2f3d
Bespi123 <62658381+Bespi123@users.noreply.github.com>
2022-04-29 11:29:22 -0500

    Add files via upload

---
+++ readFirst.m
@@ -0,0 +1,15 @@
+clc,clear;
+%%
+% Unidades
+% 
+% yaw       :       Angulo de Euler "Yaw"               en  radianes
+% pitch     :       Angulo de Euler "Pitch"             en  radianes
+% roll      :       Angulo de Euler "yaw"               en  radianes
+% velN      :       Velocidad NED respecto al Norte     en metros/segundo
+% velE      :       Velocidad NED respecto al Este      en metros/segundo
+% velD      :       Velocidad NED respecto al Down      en metros/segundo
+% alt       :       Altitud                             en metros
+% lon       :       Longitud                            en grados
+% lat       :       Latitud                             en grados
+
+load('datosActualizados.mat')
